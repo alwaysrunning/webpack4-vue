3609ae22dbe3e802cf8e6487fdf0a42b6b2f76dd
yanghaitao <yanghaitao@192.168.2.117>
2019-09-15 11:11:14 +0800

    t

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/home/index'
-import Mine from '@/mine/index'
+import Mine from '@/mine/index';
 Vue.use(Router);
 export default new Router({
     routes:[
